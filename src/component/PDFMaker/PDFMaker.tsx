16fd2c43a3248cf100704f9e6c8f3df65a237604
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2023-12-12 18:30:49 +0600

    pdf direct download

--- src/component/PDFMaker/PDFMaker.tsx
+++ src/component/PDFMaker/PDFMaker.tsx
@@ -18,6 +18,7 @@ import {
   section_name,
   subject_name,
 } from "../../utils/Utils";
+import { BsFiletypePdf } from "react-icons/bs";
 Font.register({ family: "Nikosh", src: "Nikosh.ttf", format: "truetype" });
 
 const styles = StyleSheet.create({
@@ -590,6 +591,10 @@ const RawPDFDownload = ({
   unique_id,
   teacher,
 }: any) => {
+
+
+  const pdf_name = student_info_pdf?.student_name_bn ||
+    student_info_pdf?.student_name_en + "-" + convertToBanglaNumber(student_info_pdf?.roll) + ".pdf"
   return (
     <div>
       <div>
@@ -604,10 +609,10 @@ const RawPDFDownload = ({
               teacher={teacher}
             />
           }
-          fileName="my_document.pdf"
+          fileName={pdf_name}
         >
           {({ blob, url, loading, error }: any) =>
-            loading ? "Loading document..." : "Download PDF"
+            loading ? <> <BsFiletypePdf title="loading" className="fs-4 me-2 text-secoundery" /> {"..."} </> : <BsFiletypePdf title="download" className="fs-4 me-2 text-success" />
           }
         </PDFDownloadLink>
       </div>

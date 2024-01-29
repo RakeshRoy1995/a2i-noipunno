c65846f25b3926102d23b508bd27eb533d2ee672
Arafat <shakhawat.softdev@gmail.com>
2024-01-29 18:22:13 +0600

    added

--- src/component/Attendence/StudentAttendence/StudentAttendence.tsx
+++ src/component/Attendence/StudentAttendence/StudentAttendence.tsx
@@ -85,70 +85,70 @@ const StudentAttendance = () => {
       }
     })
 
-
-  }, [classTeacherInfos, classRoomInfos])
-
-
-
+  }, [classTeacherInfos, classRoomInfos]);
+  
 
   return (
     <>
-    <Breadcumbtitle title={"শিক্ষার্থীর হাজিরা"} />
-    <section className="container" style={{ marginBottom: "200px" }}>
-      <div className="my-1 d-flex flex-column justify-content-center" style={{ fontSize: "16px", }}>
-        <label>তারিখ</label>
-        <div className="input-group">
-          <DatePicker
-            className="form-control"
-            placeholderText={"তারিখ"}
-            dateFormat="yyyy-MM-dd"
-            name="date"
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-          // readOnly
-          />
+      <Breadcumbtitle title={"শিক্ষার্থীর হাজিরা"} />
+
+      <h3 className="container my-4 text-center"> {(classTeacherInfos?.class_id == 6) && "ষষ্ঠ" || (classTeacherInfos?.class_id == 6) && "সপ্তম"} শ্রেণির হাজিরা </h3>
+
+      <section className="container" style={{ marginBottom: "200px" }}>
+        <div className="my-1 d-flex flex-column justify-content-center" style={{ fontSize: "16px", }}>
+          <label>তারিখ</label>
+          <div className="input-group">
+            <DatePicker
+              className="form-control"
+              placeholderText={"তারিখ"}
+              dateFormat="yyyy-MM-dd"
+              name="date"
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+            // readOnly
+            />
+          </div>
         </div>
-      </div>
 
-      <form onSubmit={handleSubmitAttendance}>
-        <table className="table ">
-          <thead>
-            <tr >
-              <th scope="col" className="text-center">শিক্ষার্থীর রোল</th>
-              <th scope="col">শিক্ষার্থীর নাম</th>
-              <th scope="col" className="text-center">উপস্থিতি</th>
-            </tr>
-          </thead>
-          <tbody>
-
-            {students?.map((student, key) => (
-              <tr key={key}>
-                <th scope="row" className="text-center" >{student?.roll}</th>
-                <td>{student?.student_name_bn || student?.student_name_en}</td>
-                <td className="text-center">
-                  <input
-                    style={{ height: "10px", overflowY: "auto", border: "1px solid #ccc", padding: "10px" }}
-                    className="form-check-input "
-                    type="checkbox"
-                    name={`attendance-${student?.id}`}
-                    onChange={() => handleCheckboxChange(student?.uid)}
-                  />
-                </td>
+        <form onSubmit={handleSubmitAttendance}>
+          <table className="table ">
+            <thead>
+              <tr >
+                <th scope="col" className="text-center">শিক্ষার্থীর রোল</th>
+                <th scope="col">শিক্ষার্থীর নাম</th>
+                <th scope="col" className="text-center">উপস্থিতি</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="d-flex justify-content-end align-items-center pt-1 pe-3">
-          <button
-            type="submit"
-            className="btn btn-primay px-5"
-            style={{ backgroundColor: "#428F92", color: "#fff" }}
-          >
-            জমা দিন <MdOutlineKeyboardArrowRight className="fs-3" style={{ marginTop: "-0.3rem" }} />
-          </button>
-        </div>
-      </form>
-    </section>
+            </thead>
+            <tbody>
+
+              {students?.map((student, key) => (
+                <tr key={key}>
+                  <th scope="row" className="text-center" >{student?.roll}</th>
+                  <td>{student?.student_name_bn || student?.student_name_en}</td>
+                  <td className="text-center">
+                    <input
+                      style={{ height: "10px", overflowY: "auto", border: "1px solid #ccc", padding: "10px" }}
+                      className="form-check-input "
+                      type="checkbox"
+                      name={`attendance-${student?.id}`}
+                      onChange={() => handleCheckboxChange(student?.uid)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="d-flex justify-content-end align-items-center pt-1 pe-3">
+            <button
+              type="submit"
+              className="btn btn-primay px-5"
+              style={{ backgroundColor: "#428F92", color: "#fff" }}
+            >
+              জমা দিন <MdOutlineKeyboardArrowRight className="fs-3" style={{ marginTop: "-0.3rem" }} />
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   );
 };

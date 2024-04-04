543782c8454b21939ab2853ea7c999c79797bd53
RakeshRoy1995 <rakeshroyshuvo@gmail.com>
2024-04-04 10:51:09 +0600

    first commit in branch

--- src/component/Teacher.tsx
+++ src/component/Teacher.tsx
@@ -130,7 +130,7 @@ export default function Teacher() {
         setsubject(all_subject);
         setloader(false);
 
-        await fetchData_for_restapi();
+        // await fetchData_for_restapi();
       }
     } catch (error) {
       setshowLoadingErr("");
@@ -148,6 +148,7 @@ export default function Teacher() {
     }
   };
 
+  
   const fetchData_for_restapi = async () => {
     setshowLoadingErr("");
 

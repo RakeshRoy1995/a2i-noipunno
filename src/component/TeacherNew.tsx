54b49492edf0acd27ae6e77aa23d49aee78a1952
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-05-06 11:56:42 +0600

    fixed the bug which was when there was no email the reset page becames blank, now it wont break the page

--- src/component/TeacherNew.tsx
+++ src/component/TeacherNew.tsx
@@ -181,7 +181,7 @@ export default function Teacher() {
             <div className="container">
               <div className="row">
                 <div className="col-sm-6 col-md-4 col-lg-3">
-                
+
                 </div>
                 {/* Chart */}
                 <div className="col-sm-6 col-md-4 col-lg-3">Chart</div>
